63fdf82bcf728368fa8015a809b72aa126dcf35c
Tanbin Chy <tanbinchy@gmail.com>
2026-06-09 18:18:14 +0600

    Commenting Out The Testimonials.jsx From App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import About        from './components/About';
 import Skills       from './components/Skills';
 import Projects     from './components/Projects';
 import Services     from './components/Services';
-import Testimonials from './components/Testimonials';
+// import Testimonials from './components/Testimonials';
 import Contact      from './components/Contact';
 import Footer       from './components/Footer';
 
